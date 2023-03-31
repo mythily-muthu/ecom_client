cbeaf6266cce61dd39a24624f51f01189fe1b5f5
mythily-muthu <mythilimuthu18@gmail.com>
2023-03-31 16:34:04 +0530

    added needed files and folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
+import { Route, Router } from "react-router";
 import "./App.css";
+import Product from "./components/Product";
+import Cart from "./Pages/Cart";
+import Home from "./Pages/Home";
+import ProductList from "./Pages/ProductList";
+import Success from "./Pages/Success";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h2>hee</h2>
-    </div>
+    <Router>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/produts/:category">
+        <ProductList />
+      </Route>
+      <Route path="/product/:id">
+        <Product />
+      </Route>
+      <Route path="/cart">
+        <Cart />
+      </Route>
+      <Route path="/success">
+        <Success />
+      </Route>
+    </Router>
   );
-}
+};
 
 export default App;
